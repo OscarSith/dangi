0dab16fb4ed67a9351406c101b043d40b3232b79
Oscar Larriega - Personal Macbook Pro <larriega@gmail.com>
2015-11-27 02:05:46 -0500

    Trabajos recientes, al terminar de cargar la web se muestra solo los de web

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /* <![CDATA[ */
  jQuery(document).ready(function($) {
  'use strict';
-	
+
 
 	//-------------------- Header Secrch  --------------------//
 	jQuery(".header-search-a").on('click',(function (){
@@ -19,9 +19,9 @@
 	var jQueryheaderTop = jQueryheader.offset().top;
 
 	jQuery('.offset').height( jQueryheader.outerHeight() )
-  
+
 	//-- Window Scroll Functions --
-   
+
 	jQuery(window).scroll(function(){
 		(jQuery(window).scrollTop() > jQueryheaderTop) ? jQuery('.header').addClass('fixedHeader') : jQuery('.header').removeClass('fixedHeader');
 	});
@@ -33,47 +33,47 @@
 		  if(jQuery(this).parent().has("div")) {
 			e.preventDefault();
 		  }
-		  
+
 		  if(!jQuery(this).hasClass("activeLine")) {
 			// hide any open menus and remove all other classes
 			jQuery("#accordianShortCode .accordionRow > a").removeClass("activeLine");
 			jQuery("#accordianShortCode .accordionRow > .accordion-content").removeClass("opened");
 			jQuery("#accordianShortCode .accordionRow > .accordion-content").slideUp(500);
-			
+
 			// open our new menu and add the activeLine class
 			jQuery(this).addClass("activeLine");
 			jQuery("#accordianShortCode .accordionRow > .accordion-content").addClass("opened");
 			jQuery(this).next(".accordion-content").slideDown(500);
 		  }
-		  
+
 		  else if(jQuery(this).hasClass("activeLine")) {
 			jQuery(this).removeClass("activeLine");
 			jQuery("#accordianShortCode .accordionRow > .accordion-content").removeClass("opened");
 			jQuery(this).next(".accordion-content").slideUp(500);
 		  }
 		});
-	
+
 	//--------------------------------------------------------------------------------------------
 
 	//-- Accordion 2 --
-	
+
 		jQuery("#accordianShortCode2 .accordionRow > a").on("click", function(e){
 			if(jQuery(this).parent().has("div")) {
 			e.preventDefault();
 		  }
-		  
+
 		  if(!jQuery(this).hasClass("activeLine")) {
 			// hide any open menus and remove all other classes
 			jQuery("#accordianShortCode2 .accordionRow > a").removeClass("activeLine");
 			jQuery("#accordianShortCode2 .accordionRow > .accordion-content").removeClass("opened");
 			jQuery("#accordianShortCode2 .accordionRow > .accordion-content").slideUp(500);
-			
+
 			// open our new menu and add the activeLine class
 			jQuery(this).addClass("activeLine");
 			jQuery("#accordianShortCode2 .accordionRow > .accordion-content").addClass("opened");
 			jQuery(this).next(".accordion-content").slideDown(500);
 		  }
-		  
+
 		  else if(jQuery(this).hasClass("activeLine")) {
 			jQuery(this).removeClass("activeLine");
 			jQuery("#accordianShortCode2 .accordionRow > .accordion-content").removeClass("opened");
@@ -85,11 +85,11 @@
 
 	//-- tabs --
 
-	
+
 		jQuery('.tabsContainer').easytabs();
 
   //--------------------------------------------------------------------------------------------
-  
+
 	//-- Progressbar --
 
 		jQuery('.progress-bar').on('each',(function(){
@@ -100,12 +100,12 @@
 			jQuery(this).addClass("animated slideRightSlow");
 		  }
 		}));
-	 
 
 
-	
- 
-	
+
+
+
+
 
 		//-- Including the main nav contents in responsive main nav DIV --
 		jQuery('.mainNav .navTabs').clone().appendTo('.responsiveMainNav');
@@ -119,11 +119,11 @@
 
 
 		  if ( jQuery('#responsiveMainNavToggler i').hasClass('fa-bars') )
-		  {   
+		  {
 			  jQuery('#responsiveMainNavToggler i').removeClass('fa-bars');
 			  jQuery('#responsiveMainNavToggler i').addClass('fa-close');
 		  }else
-		  {  
+		  {
 			  jQuery('#responsiveMainNavToggler i').removeClass('fa-close');
 			  jQuery('#responsiveMainNavToggler i').addClass('fa-bars');
 		  }
@@ -138,17 +138,17 @@
 		  if(jQuery(this).parent().has("ul")) {
 			e.preventDefault();
 		  }
-		  
+
 		  if(!jQuery(this).hasClass("activeLine")) {
 			// hide any open menus and remove all other classes
 			jQuery(".responsiveMainNav .navTabs > li > .toggleResponsive").removeClass("activeLine");
 			jQuery(".responsiveMainNav .navTabs > li > .dropDown").slideUp(500);
-			
+
 			// open our new menu and add the activeLine class
 			jQuery(this).addClass("activeLine");
 			jQuery(this).next(".responsiveMainNav .navTabs > li > .dropDown").slideDown(500);
 		  }
-		  
+
 		  else if(jQuery(this).hasClass("activeLine")) {
 			jQuery(this).removeClass("activeLine");
 			jQuery(this).next(".responsiveMainNav .navTabs > li > .dropDown").slideUp(500);
@@ -172,19 +172,19 @@
 			// hide any open menus and remove all other classes
 			jQuery(".responsiveMainNav .navTabs > li > .dropDown > li > .toggleResponsive").removeClass("activeLine");
 			jQuery(".responsiveMainNav .navTabs > li > .dropDown li .dropDown").slideUp(500);
-			
+
 			// open our new menu and add the activeLine class
 			jQuery(this).addClass("activeLine");
 			jQuery(this).next(".responsiveMainNav .navTabs > li > .dropDown li .dropDown").slideDown(500);
 		  }
-		  
+
 		  else if(jQuery(this).hasClass("activeLine")) {
 			jQuery(this).removeClass("activeLine");
 			jQuery(this).next(".responsiveMainNav .navTabs > li > .dropDown li .dropDown").slideUp(500);
 		  }
 		});
 
-	
+
 	//-------------------- Owlslider Testimonials1 --------------------//
 	$("#owl-testimonials").owlCarousel({
 		  slideSpeed : 300,
@@ -223,7 +223,7 @@
 
 	//-------------------- Owlslider OurClient --------------------//
 	$("#owl-OurClient").owlCarousel({
-	
+
 	  autoPlay: 3000, //Set AutoPlay to 3 seconds
       itemsCustom : [
 				[0, 1],
@@ -239,7 +239,7 @@
 	  pagination :false
   });
   //--------------------  Owlslider Blog Section --------------------//
-  
+
 	$("#owl-latest-post").owlCarousel({
       navigation : true,
 	  slideSpeed : 300,
@@ -327,7 +327,7 @@
 	  //Pagination
 	  pagination :false
 	});
-	
+
 	//--------------------  Owlslider Blog page --------------------//
 
 	$("#owl-blogpage-postslide").owlCarousel({
@@ -343,7 +343,7 @@
         "<i class='arrow_carrot-right' style='color:#7e8082;font-size: 2em;margin: 0px -5px;'></i>"
 		]
 	});
-	
+
 	//--------------------  Owlslider Portfolio Page --------------------//
 	$("#owl-porsingle-postslide").owlCarousel({
 		slideSpeed : 300,
@@ -359,57 +359,50 @@
         "<i class='arrow_carrot-right' style='color:#7e8082;font-size: 2em;margin: 0px -5px;'></i>"
 		]
 	});
-	
+
 	//-------------------- for portfoli filter jquary --------------------//
 
 		var $container = $('.portfolioContainer');
-		$container.isotope({
-			filter: '*',
-			animationOptions: {
-				duration: 750,
-				easing: 'linear',
-				queue: false
-			}
-		});
-	 
-		$('.portfolioFilter a').on('click',(function(){
-			$('.portfolioFilter .current').removeClass('current');
-			$(this).addClass('current');
-	 
-			var selector = $(this).attr('data-filter');
-			$container.isotope({
-				filter: selector,
-				animationOptions: {
-					duration: 750,
-					easing: 'linear',
-					queue: false
-				}
-			 });
-			 return false;
-		})); 
+		if ($container.length) {
+			$('.portfolioFilter a').on('click',function() {
+				$('.portfolioFilter .current').removeClass('current');
+				$(this).addClass('current');
+
+				var selector = $(this).attr('data-filter');
+				$container.isotope({
+					filter: selector,
+					animationOptions: {
+						duration: 750,
+						easing: 'linear',
+						queue: false
+					}
+				 });
+				 return false;
+			});
+		}
 		//-------------------- for portfoli lightbox jquary --------------------//
-		
+
 		jQuery(function() {
 			var $chosenSheet,
 			$stylesheets = $( "a[id^=theme-]" );
-			
+
 			// run rlightbox
 			$( ".lb" ).rlightbox();
 			$( ".lb_title-overwritten" ).rlightbox({overwriteTitle: true});
 		});
 
-	
+
     if(is_touch_device()){
-        $(".portfolio-image").on('click', function(e){					
+        $(".portfolio-image").on('click', function(e){
             $(this).find('.portfolio-hover').show();
         });
     }
-    
+
     function is_touch_device() {
-        return 'ontouchstart' in window // works on most browsers 
+        return 'ontouchstart' in window // works on most browsers
         || 'onmsgesturechange' in window; // works on ie10
     }
-	
+
 
     (function() {
         //ISOTOPE
@@ -424,43 +417,43 @@
             }
         });
 
-        
+
     })();
 	//-------------------- End PORTFOLIO ISOTOPE FILTER --------------------//
-	
+
 	//-------------------- Animation elements with CSS3 --------------------//
-	
+
 	var topOffset = $(window).scrollTop() + ($(window).height()*0.8);
-		
+
 	$('.animation, .animation-visible').on('each',(function() {
 		var imagePos = $(this).offset().top;
 		if (imagePos < topOffset) { $(this).addClass('animated ' + $(this).attr('data-animation')); }
 	}));
-	
+
 	$(window).scroll(function() {
 		var topOffset = $(window).scrollTop() + ($(window).height()*0.8);
-		
+
 		$('.animation, .animation-visible').on('each',(function() {
 			var imagePos = $(this).offset().top;
 			if (imagePos < topOffset) { $(this).addClass('animated ' + $(this).attr('data-animation')); }
 		}));
 	});
-	
+
 	$(window).resize(function() {
 		$(window).scroll(function() {
 			var topOffset = $(window).scrollTop() + ($(window).height()*0.8);
-			
+
 			$('.animation, .animation-visible').on('each',(function() {
 				var imagePos = $(this).offset().top;
 				if (imagePos < topOffset) { $(this).addClass('animated ' + $(this).attr('data-animation')); }
 			}));
 		});
 	});
-	
+
 	//-------------------- End animation elements with CSS3 --------------------//
 
 	//-------------------- Twitter integration with jQuery --------------------//
-	
+
 	$.getJSON('includes/get-tweets.php',
         function(feeds) {
             // alert(feeds);
@@ -470,7 +463,7 @@
             var feedHTML			= '';
             var displayCounter		= 1;
 			var $tweets				= $('.nd_tweets_widget');
-			
+
 			if(feeds !== null) {
 				for (var i=0; i<feeds.length; i++) {
 					var tweetscreenname	= feeds[i].user.name;
@@ -480,7 +473,7 @@
 					var isaretweet		= false;
 					var isdirect		= false;
 					var tweetid			= feeds[i].id_str;
-	 
+
 					// If the tweet has been retweeted, get the profile pic of the tweeter
 					if (typeof feeds[i].retweeted_status !== 'undefined') {
 						profileimage	= feeds[i].retweeted_status.user.profile_image_url_https;
@@ -489,21 +482,21 @@
 						tweetid			= feeds[i].retweeted_status.id_str;
 						isaretweet		= true;
 					}
-					
+
 					// Check to see if the tweet is a direct message
 					if (feeds[i].text.substr(0,1) === '@') {
 						isdirect = true;
 					}
-					
+
 					// console.log(feeds[i]);
-					
+
 					if (((showretweets === true) || ((isaretweet === false) && (showretweets === false))) && ((showdirecttweets === true) || ((showdirecttweets === false) && (isdirect === false)))) {
 						if ((feeds[i].text.length > 1) && (displayCounter <= displaylimit)) {
-	 
+
 							if (displayCounter === 1) {
 								feedHTML = '';
 							}
-							
+
 							feedHTML	+= '<li>';
 							feedHTML	+= '<div class="ndtwitter">';
 							feedHTML	+= '<a href="http://twitter.com/' + tweetusername + '/status/' + tweetid + '" target="_blank"><i class="social_twitter"></i></a>';
@@ -513,12 +506,12 @@
 								feedHTML += '<div class="date"><a href="http://twitter.com/' + tweetusername + '/status/' + tweetid + '" target="_blank">' +  JQTWEET.timeAgo(feeds[i].created_at) + '</a></div>';
 							}
 							feedHTML	+= '</li>';
-							
+
 							displayCounter++;
 						}
 					}
 				}
-	 
+
 				$tweets.html(feedHTML);
 				$tweets.hide().fadeIn(1000);
 			}
@@ -529,18 +522,18 @@
 		timeAgo: function(dateString) { // twitter date string format function
 			var rightNow = new Date();
 			var then = new Date(dateString);
-			
+
 			if (navigator.userAgent.match(/MSIE ([0-9]+)\./)) {
 				jQuery.browser.msie = true;
 				jQuery.browser.version = RegExp.$1;
 			}
-			
+
 			var diff = rightNow - then;
 			var second = 1000,
 			minute = second * 60,
 			hour = minute * 60,
 			day = hour * 24;
-	 
+
 			if (isNaN(diff) || diff < 0) { return ""; }
 			if (diff < second * 2) { return "right now"; }
 			if (diff < minute) { return Math.floor(diff / second) + " seconds ago"; }
@@ -552,7 +545,7 @@
 			if (diff < day * 365) { return Math.floor(diff / day) + " days ago"; }
 			else { return "over a year ago"; }
 		}, // timeAgo()
-		 
+
 		ify: {
 			link: function(tweet) { // twitter link string replace function
 				return tweet.replace(/\b(((https*\:\/\/)|www\.)[^\"\']+?)(([!?,.\)]+)?(\s|$))/g, function(link, m1, m2, m3, m4) {
@@ -560,35 +553,35 @@
 					return '<a class="twtr-hyperlink" target="_blank" href="' + http + m1 + '">' + ((m1.length > 25) ? m1.substr(0, 24) + '...' : m1) + '</a>' + m4;
 				});
 			},
-			
+
 			at: function(tweet) { // twitter at (@) character format function
 				return tweet.replace(/\B[@@]([a-zA-Z0-9_]{1,20})/g, function(m, username) {
 					return '<a target="_blank" class="twtr-atreply" href="http://twitter.com/intent/user?screen_name=' + username + '">@' + username + '</a>';
 				});
 			},
-			
+
 			list: function(tweet) { // twitter list string format function
 				return tweet.replace(/\B[@@]([a-zA-Z0-9_]{1,20}\/\w+)/g, function(m, userlist) {
 					return '<a target="_blank" class="twtr-atreply" href="http://twitter.com/' + userlist + '">@' + userlist + '</a>';
 				});
 			},
-			
+
 			hash: function(tweet) { // twitter hash (#) string format function
 				return tweet.replace(/(^|\s+)#(\w+)/gi, function(m, before, hash) {
 					return before + '<a target="_blank" class="twtr-hashtag" href="http://twitter.com/search?q=%23' + hash + '">#' + hash + '</a>';
 				});
 			},
-			
+
 			clean: function(tweet) { // twitter clean all string format function
 				return this.hash(this.at(this.list(this.link(tweet))));
 			}
 		} // ify
 	};
-	
+
 	//-------------------- End twitter integration with jQuery --------------------//
-	
+
 	//-------------------- Start Go Up --------------------//
-	
+
 	jQuery(window).scroll(function () {
 		if(jQuery(this).scrollTop() > 200 ) {
 			jQuery(".go-up").css("right","20px");
@@ -600,25 +593,19 @@
 		jQuery("html,body").animate({scrollTop:0},500);
 		return false;
 	}));
-	
+
 	/* ==============================================
 	Preloader
 	=============================================== */
 
 $(window).load(function(){
-    $("#preloader").delay(500).fadeOut(1000);
-        $(".preload-logo").addClass('fadeOutLeft');
-		$(".back-logo").addClass('fadeOutRight');
-		$(".preload-gif").addClass('fadeOutUp');
-    
+	$("#preloader").delay(500).fadeOut(1000);
+	$(".preload-logo").addClass('fadeOutLeft');
+	$(".back-logo").addClass('fadeOutRight');
+	$(".preload-gif").addClass('fadeOutUp');
+	$('.portfolioFilter a.current').click()
+});
 });
 
 
-
-
-	//-------------------- End Go Up --------------------//
-	
-});
-	
-	
  /* ]]> */
